906b2fc2096513052891bd805f6e3bcbf57772a1
farhan8728 <farhanmasuk.cse@gmail.com>
2022-06-21 15:26:42 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    // autoplay:1000,
+    responsive:{
+        0:{
+            items:1,
+            nav:true,
+            dots:false
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:1,
+            nav:false
+        }
+    }
+})
